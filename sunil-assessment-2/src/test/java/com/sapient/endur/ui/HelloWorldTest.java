cf0216eab2b309426da76cb7ac50160902da6a96
polsunil <polsunil@WKWZTLpOAB3XNpx.global.publicisgroupe.net>
2022-04-27 17:09:05 +0530

    first commit

---
+++ sunil-assessment-2/src/test/java/com/sapient/endur/ui/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.sapient.endur.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
